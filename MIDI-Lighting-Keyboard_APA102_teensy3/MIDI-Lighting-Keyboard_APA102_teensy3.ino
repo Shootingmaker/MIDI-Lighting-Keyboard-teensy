077684c69a5634e83acb283e801ae080ee5a7d01
Shootingmaker <shootingmaker@gmail.com>
2019-04-29 14:33:12 +0900

    Stable Version
    Teensy3.2
    72Mhz
    USB-MIDI

--- MIDI-Lighting-Keyboard_APA102_teensy3/MIDI-Lighting-Keyboard_APA102_teensy3.ino
+++ MIDI-Lighting-Keyboard_APA102_teensy3/MIDI-Lighting-Keyboard_APA102_teensy3.ino
@@ -2,41 +2,46 @@
 [MIDI Lighting Keyboard Project.]
 Kotohiro Masdua. 2019/2/14
 -----------------------------------------------------------------------------*/
-//[Library needs]
-//MIDI
-// https://github.com/FortySevenEffects/arduino_midi_library
-//MIDIcontoroller
-// https://github.com/joshnishikawa/MIDIcontroller
-//Teensyduino
-// https://www.pjrc.com/teensy/teensyduino.html
+/*
+[Library needs]
+ Arduiono MIDI
+https://github.com/FortySevenEffects/arduino_midi_library
+MIDIcontoroller
+https://github.com/joshnishikawa/MIDIcontroller
+Teensyduino
+https://www.pjrc.com/teensy/teensyduino.html
 
-//[Setting] 
-//board Teensy3.5
-//tool -> USB Type -> TeensyMIDI 
-//CPU:120Mhz
-//Faster with LTO
+ [Setting] 
+ Board Teensy3.5
+ tool -> USB Type -> TeensyMIDI 
+ CPU:120Mhz
+ Faster with LTO
 
 //[Edit some libraly]
+ first understand.
+ "MIDI.h" with MIDIcontoroller with TeensyMIDI
 
-//"MIDI.h" with MIDIcontoroller with TeensyMIDI
-// Change it
-// [midi_Settings.h]  
-// -> static const unsigned SysExMaxSize = 50000;
-// -> static const bool UseRunningStatus = true;
+ Change it
+ 
+ "midi_Settings.h" 
+ -> static const unsigned SysExMaxSize = 50000;
+ -> static const bool UseRunningStatus = true;
 
-// MIDIcontoroller
-// Change it
-// [MIDIbutton.h]   - >#include "Flicker.h  =>  #include "Flicker/Flicker.h"
+ "MIDIcontoroller"
+ Change it
+ [MIDIbutton.h]   - > #include "Flicker.h  =>  #include "Flicker/Flicker.h"
 
-// [Memo]
-// How to Using USB MIDI
-// https://www.pjrc.com/teensy/td_midi.html
+ [Memo]
+ How to Using USB MIDI
+ https://www.pjrc.com/teensy/td_midi.html
 
-//[to LED(APA102) Wireing]
-// 5V & GND
-// MOSI is  11
-// CLK  is  13
+[Wireing to LED(APA102)]
+ 5V    Almost needs 1000mA with Capacitor 100uF is Better.
+ GND   Wide line.
+ MOSI is  11
+ CLK  is  13
 
+-----------------------------------------------------------------------------*/
 
 #include "MIDIcontroller.h"
 
